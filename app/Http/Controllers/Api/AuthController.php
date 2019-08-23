0539b93440276922ba75e0239d03d29fb2015e9f
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2019-08-23 15:01:28 -0300

    sso register ok

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    { 
+        $validatedData = $request->validate([
+            'name'=>'required|max:55',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|confirmed',
+
+        ]);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user'=> $user,'access_token'=>$accessToken]);
+         
+
+    }
+    public function login()
+    { }
+}
